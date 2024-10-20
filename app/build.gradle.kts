179541bc89c7c0389fe26daa261483d4f9977f3d
egoriudin <egor3352@yandex.ru>
2024-10-21 00:01:12 +0500

    downgrade android api to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.flasska.finapp"
-        minSdk = 30
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
